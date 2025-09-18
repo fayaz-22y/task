4af583fc3bf0c6c27f1ee8694ae3565b4fa979f5
fayaz-22y <mohammadfayaz22y@gmail.com>
2025-09-18 18:08:07 +0530

    javascript files

---
+++ task/javascript/javascript10.js
@@ -0,0 +1,14 @@
+// simple.js
+
+// A simple function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Call the function
+console.log(greet("World"));
+
+// A small loop
+for (let i = 1; i <= 5; i++) {
+  console.log("Number:", i);
+}
