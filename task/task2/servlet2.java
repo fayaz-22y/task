abb16a1f9d517ab543661ff2cdd7060f3d363e27
fayaz-22y <mohammadfayaz22y@gmail.com>
2025-09-18 18:16:27 +0530

    new java file

---
+++ task/task2/servlet2.java
@@ -0,0 +1,12 @@
+// Simple.java
+
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // A small loop
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
